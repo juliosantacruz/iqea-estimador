7382d6f283a416d1edf8fb0bf6b86b9a3b2f3564
Julio Santacruz <julio.santacruzh@gmail.com>
2024-01-29 21:30:09 -0800

    deploy test

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.scss";
 // import './styles/mixins.scss'
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import MainLayout from "./layout/MainLayout.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MainLayout>
         <App />
       </MainLayout>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
